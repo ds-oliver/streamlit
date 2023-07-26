e26a7c122874df4c1919ca1645ec5eb38cc7d532
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-07-26 16:21:36 -0700

    x

--- pages/5_test_updates_h2dpage.py
+++ pages/5_test_updates_h2dpage.py
@@ -90,16 +90,16 @@ def get_top_players(team, player_df, stat, top=5):
     if 'team' not in player_df.columns:
         raise ValueError('team column not in player_df')
     else:
-        print('team column in player_df')
-        top_players_matchup = player_df.groupby(['matchup_merge_key', 'team']).agg({stat: 'sum'}).reset_index()
-        top_players_matchup = top_players_matchup[top_players_matchup['team'] == team].sort_values(by=stat, ascending=False).head(top)
+        # Top players by matchup
+        top_players_matchup = player_df[player_df['team'] == team].groupby(['player', 'matchup_merge_key']).agg({stat: 'sum'}).reset_index()
+        top_players_matchup = top_players_matchup.sort_values(by=stat, ascending=False).groupby('player').first().head(top)
 
-        # Top players by season_merge_key
-        top_players_season = player_df.groupby(['season_merge_key']).agg({stat: 'sum'}).reset_index()
-
-        top_players_season = top_players_season[top_players_season['team'] == team].sort_values(by=stat, ascending=False).head(top)
+        # Top players by season
+        top_players_season = player_df[player_df['team'] == team].groupby(['player', 'season_merge_key']).agg({stat: 'sum'}).reset_index()
+        top_players_season = top_players_season.sort_values(by=stat, ascending=False).groupby('player').first().head(top)
 
     return top_players_matchup, top_players_season
+
 
 def get_teams_stats(df, team1, team2):
     """
