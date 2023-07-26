649dc197de56045f92628e3f9cae46b917795b96
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-07-26 10:39:27 -0700

    Plotting updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,20 @@ st.title('My first app')
 
 st.write("Here's our first attempt at using data to create a table:")
 
-df = pd.read_csv('all_seasons_combined_df_2023-07-24_14-08-09.csv')
+df = pd.read_csv('/Users/hogan/Library/CloudStorage/Dropbox/Mac/Documents/GitHub/streamlit/all_seasons_combined_df_2023-07-25_12-50-09.csv')
 
 st.write(df)
 
 # plot the data
 
-st.line_chart(df)
+# columns_to_chart = st.multiselect(
+#     label='What columns do you want to chart?',
+#     options=df.columns)
+
+# st.line_chart(df[columns_to_chart])
+
+# select a player or team
+
+player_or_team = st.radio(
+    label='Player or Team?',
+    options=['Player', 'Team'])
