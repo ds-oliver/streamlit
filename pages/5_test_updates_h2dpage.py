07dd46def2faecd51d137d4310ab77a02d8983f7
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-07-26 16:16:39 -0700

    z

--- pages/5_test_updates_h2dpage.py
+++ pages/5_test_updates_h2dpage.py
@@ -91,6 +91,7 @@ def get_top_players(team, player_df, stat, top=5):
         raise ValueError('team column not in player_df')
     else:
         print('team column in player_df')
+        top_players_matchup = player_df.groupby(['matchup_merge_key', 'team', 'player']).agg({stat: 'sum'}).reset_index()
         top_players_matchup = player_df[player_df['team'] == team].sort_values(by=stat, ascending=False).head(top)
 
         # Top players by season_merge_key
