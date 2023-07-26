dcc1b7c5bd946075c198ce4fdacab0e0e2841dbb
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-07-26 16:55:11 -0700

    new page for test

--- pages/6_test_again.py
+++ pages/6_test_again.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import re
 import uuid
+from sklearn.preprocessing import StandardScaler
 
 def load_player_data():
     player_df = pd.read_csv('data/all_seasons_combined_df_2023-07-25_12-50-09.csv')
@@ -11,7 +12,14 @@ def load_player_data():
     player_df = player_df.drop(drop_cols, axis=1)
     return player_df
 
+def standardize(df):
+    numeric_cols = df.select_dtypes(include=[np.number]).columns.tolist()
+    scaler = StandardScaler()
+    df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
+    return df
+
 def process_player_data(player_df):
+    player_df = standardize(player_df)
     player_df['year'] = player_df['season'].str[:4]
     player_df = player_df.rename(columns={'season': 'season_long', 'year': 'season', 'position_1': 'position'})
 
@@ -26,14 +34,8 @@ def process_player_data(player_df):
     player_df['home_team'] = np.select(conditions, choices_team)
     player_df['away_team'] = np.select(conditions, choices_opponent)
 
-    # create merge_key which is home_team + away_team + year
     player_df['matchup_merge_key'] = player_df.apply(lambda row: '_'.join(sorted([row['home_team'], row['away_team']])), axis=1)
-
-    # create season_merge_key which is sorted home_team + away_team + season
     player_df['season_merge_key'] = player_df.apply(lambda row: '_'.join(sorted([row['home_team'], row['away_team']] + [row['season']])), axis=1)
-
-
-    # player_df['season_merge_key'] = player_df['matchup_merge_key'] + player_df['season']
 
     player_df['season_gameweek'] = player_df['season'] + '_' + player_df['gameweek'].astype(str)
     cols_order = ['player', 'team', 'season_gameweek', 'minutes', 'position'] + [col for col in player_df.columns if col not in ['player', 'team', 'season_gameweek', 'minutes', 'position']]
@@ -42,82 +44,34 @@ def process_player_data(player_df):
     return player_df
 
 def clean_dataframes(df):
-    """Description: This function cleans the dataframes by removing unnecessary columns, reordering columns, creating new columns and renaming columns.
-    
-        Arguments:
-            df {dataframe} -- dataframe to be cleaned
-        
-        Returns:
-            df {dataframe} -- cleaned dataframe
-
-    """
     drop_cols = ['score', 'match_report', 'notes']
     df = df.drop(drop_cols, axis=1)
 
-    # reorder columns gameweek, home_team, home_score, home_xg, away_team, away_score, away_xg, date, season
     df = df[['gameweek', 'home_team', 'home_score', 'home_xg', 'away_team', 'away_score', 'away_xg', 'date', 'season']]
 
-    # create a column for the winning_team, if draw then draw
     df['winning_team'] = np.where(df['home_score'] > df['away_score'], df['home_team'], np.where(df['home_score'] < df['away_score'], df['away_team'], 'draw'))
-
-    # create a column for the losing_team, if draw then draw
     df['losing_team'] = np.where(df['home_score'] < df['away_score'], df['home_team'], np.where(df['home_score'] > df['away_score'], df['away_team'], 'draw'))
 
-    # create match_id column which is applymap of home_team and away_team and season using uuid
     df['match_id'] = df[['home_team', 'away_team', 'season']].applymap(str).apply(lambda x: ''.join(x), axis=1).apply(lambda x: uuid.uuid5(uuid.NAMESPACE_DNS, x))
-
     df['matchup_merge_key'] = df[['home_team', 'away_team']].applymap(str).apply(lambda x: ''.join(x), axis=1).apply(lambda x: uuid.uuid5(uuid.NAMESPACE_DNS, x))
     df['season_merge_key'] = df[['home_team', 'away_team', 'season']].applymap(str).apply(lambda x: ''.join(x), axis=1).apply(lambda x: uuid.uuid5(uuid.NAMESPACE_DNS, x))
 
-    # ifna() 0
     df = df.fillna(0)
 
     return df
 
 def get_top_players(team, player_df, stat, top=5):
-    """
-    Get the top players from a team for a given statistic.
-
-    Parameters:
-    team (str): The team to get players from.
-    player_df (DataFrame): The player data.
-    stat (str): The statistic to rank players by.
-    top (int): The number of top players to return.
-
-    Returns:
-    DataFrame: A DataFrame with the top players and their stats.
-    """
-    # check if team column exists in player_df
     if 'team' not in player_df.columns:
         raise ValueError('team column not in player_df')
     else:
-        # Filter dataframe based on team
         player_df_team = player_df[player_df['team'] == team]
 
-        # Top players by matchup
         top_players_matchup = player_df_team.groupby(['player']).agg({stat: 'sum'}).sort_values(by=stat, ascending=False).head(top)
-
-        # Top players by season
         top_players_season = player_df_team.groupby(['player']).agg({stat: 'sum'}).sort_values(by=stat, ascending=False).head(top)
 
     return top_players_matchup, top_players_season
 
-
-
-
 def get_teams_stats(df, team1, team2):
-    """
-    Get the stats for two teams for a specific matchup_merge_key.
-
-    Parameters:
-    df (DataFrame): The DataFrame to get the stats from.
-    team1 (str): The first team to get stats for.
-    team2 (str): The second team to get stats for.
-
-    Returns:
-    dict, dict: Two dictionaries with the statistics for the two teams.
-    """
-    # Initialize stats dictionaries
     stats_team1 = {
         'total_games': 0,
         'total_wins': 0,
@@ -139,7 +93,6 @@ def get_teams_stats(df, team1, team2):
         'Clean Sheets': 0
     }
 
-    # Filter df for games with team1 and team2
     df_filtered = df[(df['home_team'].isin([team1, team2])) & (df['away_team'].isin([team1, team2]))]
 
     for index, row in df_filtered.iterrows():
@@ -149,31 +102,50 @@ def get_teams_stats(df, team1, team2):
             stats_team1['total_goals_conceded'] += row['away_score']
             stats_team1['xG For'] += row['home_xg']
             stats_team1['xG Against'] += row['away_xg']
-            if row['away_score'] == 0:
-                stats_team1['Clean Sheets'] += 1
+            stats_team1['Clean Sheets'] += 1 if row['away_score'] == 0 else 0
             if row['winning_team'] == team1:
                 stats_team1['total_wins'] += 1
-            elif row['winning_team'] == 'draw':
-                continue
-            else:
+            elif row['losing_team'] == team1:
                 stats_team1['total_losses'] += 1
-                
+
+        if row['away_team'] == team1:
+            stats_team1['total_games'] += 1
+            stats_team1['total_goals_scored'] += row['away_score']
+            stats_team1['total_goals_conceded'] += row['home_score']
+            stats_team1['xG For'] += row['away_xg']
+            stats_team1['xG Against'] += row['home_xg']
+            stats_team1['Clean Sheets'] += 1 if row['home_score'] == 0 else 0
+            if row['winning_team'] == team1:
+                stats_team1['total_wins'] += 1
+            elif row['losing_team'] == team1:
+                stats_team1['total_losses'] += 1
+
         if row['home_team'] == team2:
             stats_team2['total_games'] += 1
             stats_team2['total_goals_scored'] += row['home_score']
             stats_team2['total_goals_conceded'] += row['away_score']
             stats_team2['xG For'] += row['home_xg']
             stats_team2['xG Against'] += row['away_xg']
-            if row['away_score'] == 0:
-                stats_team2['Clean Sheets'] += 1
+            stats_team2['Clean Sheets'] += 1 if row['away_score'] == 0 else 0
             if row['winning_team'] == team2:
                 stats_team2['total_wins'] += 1
-            elif row['winning_team'] == 'draw':
-                continue
-            else:
+            elif row['losing_team'] == team2:
+                stats_team2['total_losses'] += 1
+
+        if row['away_team'] == team2:
+            stats_team2['total_games'] += 1
+            stats_team2['total_goals_scored'] += row['away_score']
+            stats_team2['total_goals_conceded'] += row['home_score']
+            stats_team2['xG For'] += row['away_xg']
+            stats_team2['xG Against'] += row['home_xg']
+            stats_team2['Clean Sheets'] += 1 if row['home_score'] == 0 else 0
+            if row['winning_team'] == team2:
+                stats_team2['total_wins'] += 1
+            elif row['losing_team'] == team2:
                 stats_team2['total_losses'] += 1
 
     return stats_team1, stats_team2
+
 
 def show_head2head_analysis(df_all_seasons, player_df):
     # Create a list of teams
@@ -209,19 +181,6 @@ def show_head2head_analysis(df_all_seasons, player_df):
         selected_stats = default_stats
         
     st.dataframe(player_df)
-
-
-# Get top 5 players for each team
-    for stat in selected_stats:
-        top5_team1, top5_season_team1 = get_top_players(team_selection1, player_df, stat, top=5)
-        top5_team2, top5_season_team2 = get_top_players(team_selection2, player_df, stat, top=5)
-
-        # Display the top 5 players
-        st.subheader(f'Top 5 {team_selection1} players by {stat}:')
-        st.dataframe(top5_team1)
-        st.subheader(f'Top 5 {team_selection2} players by {stat}:')
-        st.dataframe(top5_team2)
-
 
 def main():
     # Load and process player data
