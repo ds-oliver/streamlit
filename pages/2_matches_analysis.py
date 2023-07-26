d6adf305e52376334e2b1156575addb41a3be46d
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-07-26 12:46:58 -0700

    first version

--- pages/2_matches_analysis.py
+++ pages/2_matches_analysis.py
@@ -15,6 +15,15 @@ df_2018_2019 = pd.read_csv('data/df_2023-07-25_09-57-12_2018-2019.csv')
 df_2017_2018 = pd.read_csv('data/df_2023-07-25_09-57-10_2017-2018.csv')
 
 def clean_dataframes(df):
+    """Description: This function cleans the dataframes by removing unnecessary columns, reordering columns, creating new columns and renaming columns.
+    
+        Arguments:
+            df {dataframe} -- dataframe to be cleaned
+        
+        Returns:
+            df {dataframe} -- cleaned dataframe
+
+    """
     drop_cols = ['score', 'match_report', 'notes']
     df = df.drop(drop_cols, axis=1)
 
@@ -32,122 +41,78 @@ def clean_dataframes(df):
 
     return df
 
-# create a list of dataframes
-df_list = [df_2022_2023, df_2021_2022, df_2020_2021, df_2019_2020, df_2018_2019, df_2017_2018]
-
-for df in df_list:
-    df = clean_dataframes(df)
-
 # concatenate the dataframes
 df_all_seasons = pd.concat(df_list)
 
-def show_head2head_analysis():
-    """_summary_: This is the function to show the head2head analysis
-
-    _description_: This function will be used to show the head2head analysis
-
-    _parameters_: None
-
-    _returns_: None
-
-    _example_: None
-    """
+def show_head2head_analysis(df_all_seasons):
     # create a list of seasons
     season_list = df_all_seasons['season'].unique().tolist()
 
     # create a list of teams
     team_list = df_all_seasons['home_team'].unique().tolist()
 
-    # create a multiselect for teams
-    team_selection = st.multiselect('Select teams', team_list)
+    # create two selectboxes for the two teams
+    team_selection1 = st.selectbox('Select first team', team_list)
+    team_selection2 = st.selectbox('Select second team', [team for team in team_list if team != team_selection1])
+    
+    # ensure the two selected teams are not the same
+    while team_selection1 == team_selection2:
+        st.error("Please select two different teams.")
+        team_selection2 = st.selectbox('Select second team', [team for team in team_list if team != team_selection1])
 
-    # create a multiselect for seasons
-    season_selection = st.multiselect('Select seasons', season_list)
+    # create a multiselect for seasons although seasons are not required
+    season_selection = st.multiselect('[Optional] Select season(s)', season_list)
 
     # create a dataframe of the selected teams
-    df_selected_teams = df_all_seasons[df_all_seasons['home_team'].isin(team_selection)]
+    df_selected_teams = df_all_seasons[df_all_seasons['home_team'].isin([team_selection1, team_selection2])]
 
     # create a dataframe of the selected seasons
     df_selected_seasons = df_all_seasons[df_all_seasons['season'].isin(season_selection)]
 
     # create a dataframe of the selected teams and seasons
-    df_selected_teams_seasons = df_all_seasons[df_all_seasons['home_team'].isin(team_selection) & df_all_seasons['season'].isin(season_selection)]
+    df_selected_teams_seasons = df_selected_teams[df_selected_teams['season'].isin(season_selection)]
 
-    # create a dataframe of the head2head
-    df_head2head = df_selected_teams_seasons.groupby(['home_team', 'away_team']).agg({'home_team': 'count', 'winning_team': 'count', 'losing_team': 'count'}).rename(columns={'home_team': 'total_matches', 'winning_team': 'total_wins', 'losing_team': 'total_losses'}).reset_index()
+    df_selected_teams_seasons['winner'] = np.where(df_selected_teams_seasons['home_score'] > df_selected_teams_seasons['away_score'], df_selected_teams_seasons['home_team'], df_selected_teams_seasons['away_team'])
+    df_selected_teams_seasons['loser'] = np.where(df_selected_teams_seasons['home_score'] < df_selected_teams_seasons['away_score'], df_selected_teams_seasons['home_team'], df_selected_teams_seasons['away_team'])
+    df_selected_teams_seasons.loc[df_selected_teams_seasons['home_score'] == df_selected_teams_seasons['away_score'], ['winner', 'loser']] = 'Draw'
 
-    # create a dataframe of the home wins
-    df_home_wins = df_selected_teams_seasons[df_selected_teams_seasons['winning_team'] == df_selected_teams_seasons['home_team']].groupby(['home_team', 'away_team']).agg({'home_team': 'count'}).rename(columns={'home_team': 'home_wins'}).reset_index()
+    team1_stats = {
+        'Wins': (df_selected_teams_seasons['winner'] == team_selection1).sum(),
+        'Losses': (df_selected_teams_seasons['loser'] == team_selection1).sum(),
+        'Draws': ((df_selected_teams_seasons['home_team'] == team_selection1) & (df_selected_teams_seasons['winner'] == 'Draw')).sum(),
+        'Goals For': df_selected_teams_seasons.loc[df_selected_teams_seasons['home_team'] == team_selection1, 'home_score'].sum() + df_selected_teams_seasons.loc[df_selected_teams_seasons['away_team'] == team_selection1, 'away_score'].sum(),
+        'Goals Against': df_selected_teams_seasons.loc[df_selected_teams_seasons['home_team'] == team_selection1, 'away_score'].sum() + df_selected_teams_seasons.loc[df_selected_teams_seasons['away_team'] == team_selection1, 'home_score'].sum(),
+    }
 
-    # create a dataframe of the away wins
-    df_away_wins = df_selected_teams_seasons[df_selected_teams_seasons['winning_team'] == df_selected_teams_seasons['away_team']].groupby(['home_team', 'away_team']).agg({'away_team': 'count'}).rename(columns={'away_team': 'away_wins'}).reset_index()
+    team2_stats = {
+        'Wins': (df_selected_teams_seasons['winner'] == team_selection2).sum(),
+        'Losses': (df_selected_teams_seasons['loser'] == team_selection2).sum(),
+        'Draws': ((df_selected_teams_seasons['home_team'] == team_selection2) & (df_selected_teams_seasons['winner'] == 'Draw')).sum(),
+        'Goals For': df_selected_teams_seasons.loc[df_selected_teams_seasons['home_team'] == team_selection2, 'home_score'].sum() + df_selected_teams_seasons.loc[df_selected_teams_seasons['away_team'] == team_selection2, 'away_score'].sum(),
+        'Goals Against': df_selected_teams_seasons.loc[df_selected_teams_seasons['home_team'] == team_selection2, 'away_score'].sum() + df_selected_teams_seasons.loc[df_selected_teams_seasons['away_team'] == team_selection2, 'home_score'].sum(),
+    }
 
-    # create a dataframe of the draws
-    df_draws = df_selected_teams_seasons[df_selected_teams_seasons['winning_team'] == 'draw'].groupby(['home_team', 'away_team']).agg({'winning_team': 'count'}).rename(columns={'winning_team': 'draws'}).reset_index()
+    df_head2head = pd.DataFrame({team_selection1: team1_stats, team_selection2: team2_stats})
 
-    # merge the dataframes
-    df_head2head = df_head2head.merge(df_home_wins, how='left', on=['home_team', 'away_team']).merge(df_away_wins, how='left', on=['home_team', 'away_team']).merge(df_draws, how='left', on=['home_team', 'away_team'])
-
-    # fill the NaN values with 0
-    df_head2head = df_head2head.fillna(0)
-
-    # create a column for the total goals
-    df_head2head['total_goals'] = df_head2head['home_wins'] + df_head2head['away_wins'] + df_head2head['draws']
-
-    # create a column for the home win percentage
-    df_head2head['home_win_percentage'] = df_head2head['home_wins'] / df_head2head['total_matches']
-
-    # create a column for the away win percentage
-    df_head2head['away_win_percentage'] = df_head2head['away_wins'] / df_head2head['total_matches']
-
-    # create a column for the draw percentage
-    df_head2head['draw_percentage'] = df_head2head['draws'] / df_head2head['total_matches']
-
-    # create a column for the home goals scored
-    df_head2head['home_goals_scored'] = df_selected_teams_seasons.groupby(['home_team', 'away_team']).agg({'home_score': 'sum'}).rename(columns={'home_score': 'home_goals_scored'}).reset_index()['home_goals_scored']
-
-    # create a column for the away goals scored
-    df_head2head['away_goals_scored'] = df_selected_teams_seasons.groupby(['home_team', 'away_team']).agg({'away_score': 'sum'}).rename(columns={'away_score': 'away_goals_scored'}).reset_index()['away_goals_scored']
-
-    # create a column for the home goals conceded
-    df_head2head['home_goals_conceded'] = df_selected_teams_seasons.groupby(['home_team', 'away_team']).agg({'away_score': 'sum'}).rename(columns={'away_score': 'home_goals_conceded'}).reset_index()['home_goals_conceded']
-
-    # create a column for the away goals conceded
-    df_head2head['away_goals_conceded'] = df_selected_teams_seasons.groupby(['home_team', 'away_team']).agg({'home_score': 'sum'}).rename(columns={'home_score': 'away_goals_conceded'}).reset_index()['away_goals_conceded']
-
-    # create a column for the total goals scored
-    df_head2head['total_goals_scored'] = df_head2head['home_goals_scored'] + df_head2head['away_goals_scored']
-
-    # create a column for the total goals conceded
-    df_head2head['total_goals_conceded'] = df_head2head['home_goals_conceded'] + df_head2head['away_goals_conceded']
-
-    # create a column for the goal difference
-    df_head2head['goal_difference'] = df_head2head['total_goals_scored'] - df_head2head['total_goals_conceded']
-
-    # create a column for the average goals scored
-    df_head2head['average_goals_scored'] = df_head2head['total_goals_scored'] / df_head2head['total_matches']
-
-    # create a column for the average goals conceded
-    df_head2head['average_goals_conceded'] = df_head2head['total_goals_conceded'] / df_head2head['total_matches']
-
-
+    # display the dataframe
+    st.dataframe(df_head2head)
 
 
 def main():
-    """_summary_: This is the main function for the matches analysis page
+    """_summary_: main function to run the app
 
-    _description_: This page will be used to analyse the matches data
+    _param_: None
 
-    _parameters_: None
+    _return_: None
 
-    _returns_: None
-
-    _example_: None
     """
-    for df in df_list:
-        df = clean_dataframes(df)
+    df_list_cleaned = [clean_dataframes(df) for df in df_list]
 
-    # concatenate the dataframes
-    df_all_seasons = pd.concat(df_list)
+    # concatenate the cleaned dataframes
+    df_all_seasons = pd.concat(df_list_cleaned)
 
-    # two 
+    show_head2head_analysis(df_all_seasons) 
+
+
+if __name__ == "__main__":
+    main()
