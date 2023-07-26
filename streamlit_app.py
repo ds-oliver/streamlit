28abefcdd87d0d27bdcfe3e2a6f6e611b43e2ea5
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-07-26 10:00:34 -0700

    Update
    venv update

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('My first app')
 
 st.write("Here's our first attempt at using data to create a table:")
 
-df = pd.read_csv('/Users/hogan/dev/fbref/scripts/rfx_scrape/match_results_project/premierleague/test/output_files/csv_files/all_seasons_combined_df_2023-07-24_14-08-09.csv')
+df = pd.read_csv('all_seasons_combined_df_2023-07-24_14-08-09.csv')
 
 st.write(df)
 
