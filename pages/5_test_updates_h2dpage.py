fcc39936a1ab672168b47b04d1614d936b2fbe63
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-07-26 18:47:58 -0700

    reversed

--- pages/5_test_updates_h2dpage.py
+++ pages/5_test_updates_h2dpage.py
@@ -207,7 +207,7 @@ def show_head2head_analysis(df_all_seasons, player_df):
     st.info('Select season(s) you want to compare. \nNote: Default is the most recent season')
 
     # create a multiselect for seasons to select one or many seasons but it should default to the most recent season
-    season_selection = st.multiselect('Select Season(s)', sorted(df_all_seasons['season'].unique().tolist()), default=reversed(sorted(df_all_seasons['season'].unique().tolist())[-1]))
+    season_selection = st.multiselect('Select Season(s)', reversed(sorted(df_all_seasons['season'].unique().tolist())), default=sorted(df_all_seasons['season'].unique().tolist())[-1])
 
     # Filter the DataFrame based on the season(s) selected
     df_filtered = df_all_seasons[df_all_seasons['season'].isin(season_selection)]
