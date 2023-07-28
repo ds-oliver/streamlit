bbb3f7e9762a6a8d22ccfb85255b3c2af45bf9f3
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-07-28 13:34:19 -0700

    ..scripts

--- pages/1_load_data_from_dbt.py
+++ pages/1_load_data_from_dbt.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scripts.chunk_and_save_data import load_data_from_db
+from ..scripts.chunk_and_save_data import load_data_from_db
 
 data = load_data_from_db()
 
