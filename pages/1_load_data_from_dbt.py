ccea27703486e4322ba98ea64727757afb50242c
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-07-28 13:37:37 -0700

    sys append

--- pages/1_load_data_from_dbt.py
+++ pages/1_load_data_from_dbt.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from ..scripts.chunk_and_save_data import load_data_from_db
+# 1_load_data_from_dbt.py
+import sys
+import os
+
+# Add the scripts directory to the Python path
+sys.path.append('/Users/hogan/Library/CloudStorage/Dropbox/Mac/Documents/GitHub/streamlit/scripts')
+
+from chunk_and_save_data import load_data_from_db
 
 data = load_data_from_db()
-
 st.write(data)
